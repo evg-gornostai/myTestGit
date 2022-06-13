a7ce0df0796cd78e165567997e53cab9a7531dca
evg-gornostai <harnastai750@gmail.com>
2022-06-13 21:00:34 +0300

    initial commit

---
+++ app/src/main/java/com/gornostai/mytestgit/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.gornostai.mytestgit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
