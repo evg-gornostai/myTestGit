255345907bcab95f62de15db523d4ea8d4bc3e3b
evg-gornostai <harnastai750@gmail.com>
2022-06-13 21:11:54 +0300

    sdfdsfsdfsf

--- app/src/main/java/com/gornostai/mytestgit/MainActivity.kt
+++ app/src/main/java/com/gornostai/mytestgit/MainActivity.kt
@@ -7,5 +7,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val s1: String = "first"
+        val s2: String = "first1"
     }
 }
